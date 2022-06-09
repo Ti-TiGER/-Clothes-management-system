7709cda49f291f3e3d519d06ff8f365c2bed9734
Ti-TiGER <tigerchairat1144@gmail.com>
2022-06-09 15:44:16 +0700

    Update Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,82 +1,141 @@
-import React, { useState } from "react";
-import { makeStyles, styled } from "@mui/styles";
+import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import pic from "../Component/picture/Storehouse.png";
-
-import swal from "sweetalert2";
-
-export default function Login() {
+function Copyright(props) {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Grid container spacing={2}>
-        <Grid item xs="auto">
-          <img
-            src={pic}
-            Width="1100"
-            height="1100"
-            className="img-fluid rounded "
-          />
-        </Grid>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-        <Grid item xs={12} md={5} elevation={6} square>
-          <Paper className={"paddingTop=30px"}>
-            <Avatar>
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://cdn.discordapp.com/attachments/744696824220221502/984030671427231775/unknown.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: 30,
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Login
+              Sign in
             </Typography>
-            <form>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
-                id="Email"
-                label="Username"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircle />
-                    </InputAdornment>
-                  ),
-                }}
-                fullWidth
-                required
                 margin="normal"
-                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
               />
               <TextField
-                variant="outlined"
                 margin="normal"
                 required
                 fullWidth
-                id="password"
                 name="password"
                 label="Password"
                 type="password"
-                // onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
               />
               <Button
-                paddingTop="100px"
                 type="submit"
                 fullWidth
                 variant="contained"
-                color="primary"
+                sx={{ mt: 3, mb: 2 }}
               >
-                Login
+                Sign In
               </Button>
-            </form>
-          </Paper>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
         </Grid>
       </Grid>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
